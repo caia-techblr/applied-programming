d547f7ea9a11e48d87fd83e2a18a0ef05672de68
Rajesh Sola <rsola@gitam.edu>
2025-12-29 11:26:49 +0530

    Create flightdb.py

---
+++ flightdb.py
@@ -0,0 +1,35 @@
+flightdb = [
+    [ "Indigo", "6E205", "Bengaluru", "Delhi", "1000"],
+    [ "AirXPress", "AI405", "Chennai", "Hyderabad", "500"]
+]
+
+
+def add_service(operator, fno, origin, dest, distance):
+    pass
+
+def list_services():
+    pass
+
+def list_services_by_origin(origin):
+    pass
+
+def list_services_by_destination(destination):
+    pass
+
+def list_services_by_operator(operator):
+    pass
+
+def find_service_with_max_fare():
+    pass
+
+def find_service_with_min_fare():
+    pass
+
+def find_service_with_long_duration()
+    pass
+
+def find_service_with_short_duration()
+    pass
+
+
+
