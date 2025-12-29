9310b6eaacbe288baadf5c7a29b11c7bfcb47e8d
Rajesh Sola <rsola@gitam.edu>
2025-12-29 11:56:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flightdb import *
+#from flightdb_ops import *
+from flightdb_ops import add_service
+from flightdb_ops import list_all_services
 
 if __name__ == "__main__":
-    pass   # call the functions
+    add_service("Indigo", "6E205", "Bengaluru", "Delhi", "1000")
+    add_service("AirXPress", "AI405", "Chennai", "Hyderabad", "500")
+    list_all_services()
