da33906046e1396f2edf751b5950b9f3c8639021
Rajesh Sola <rsola@gitam.edu>
2025-12-29 11:56:20 +0530

    Update flightdb.py

--- flightdb.py
+++ flightdb.py
@@ -1,17 +1,26 @@
-flightdb = [
+"""
+flightservicedb = [
     [ "Indigo", "6E205", "Bengaluru", "Delhi", "1000"],
     [ "AirXPress", "AI405", "Chennai", "Hyderabad", "500"]
 ]
+"""
+flightservicedb = []
 
+def add_service(operator, fno, origin, destination, distance):
+    service = [operator,fno,origin,destination,distance]
+    flightservicedb.append(service)
 
-def add_service(operator, fno, origin, dest, distance):
-    pass
-
-def list_services():
-    pass
+def list_all_services():
+    for service in flightservicedb:
+        print(service)
+        #print(service[0],service[1],service[2],service[3],service[4])
 
 def list_services_by_origin(origin):
-    pass
+    # results = {}
+    for service in flightservicedb:
+        if service[2] == origin:
+            print(service)  # results.add(service)
+    # return results
 
 def list_services_by_destination(destination):
     pass
@@ -25,10 +34,10 @@ def find_service_with_max_fare():
 def find_service_with_min_fare():
     pass
 
-def find_service_with_long_duration()
+def find_service_with_long_duration():
     pass
 
-def find_service_with_short_duration()
+def find_service_with_short_duration():
     pass
 
 
